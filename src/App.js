389cc84ede63d103f76b9868cf9fca2bcd4f1a50
Connor McDermott <connormcderm@protonmail.com>
2020-05-05 10:10:07 -0500

    Header with logo and links

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import { Route, Switch } from 'react-router-dom';
 import './fonts.css';
 import './App.css';
 
+import Header from './components/header/header.component';
 import HomePage from './pages/homepage/homepage.component';
 import Shop from './pages/shop/shop.component';
 
 function App() {
 	return (
 		<>
+			<Header/>
 			<Switch>
 				<Route exact path='/' component={HomePage} />
 				<Route exact path='/shop' component={Shop}/>
